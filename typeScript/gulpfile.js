e3585be1fa27d076d3c0467794a17c0154c1a374
Tulio Ferreira <tuliof210@gmail.com>
2021-05-19 09:58:52 -0300

    combine ts transform result with concat and minification

--- typeScript/gulpfile.js
+++ typeScript/gulpfile.js
@@ -1,11 +1,26 @@
 const { series } = require('gulp');
-
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
+const concat = require('gulp-concat');
+const unglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 function transformTS(callback) {
-  tsProject.src().pipe(tsProject()).pipe(gulp.dest('build'));
+  tsProject
+    .src()
+    .pipe(tsProject())
+    .pipe(
+      babel({
+        comments: false,
+        presets: ['env'],
+      })
+    )
+    .on('error', err => console.log(err))
+    .pipe(unglify())
+    .pipe(concat('ts_transform.min.js'))
+    .pipe(gulp.dest('build'));
+
   return callback();
 }
 
