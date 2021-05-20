d114c4de30992badcb9dffced32f8519521f5a82
Tulio Ferreira <tuliof210@gmail.com>
2021-05-20 13:33:56 -0300

    webserver with livereload

--- spa/gulp/server.gulp.js
+++ spa/gulp/server.gulp.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver');
 
 function watchFiles(cb) {
   return cb();
 }
-function server(cb) {
-  return cb();
+function server() {
+  return gulp.src('build').pipe(
+    webserver({
+      port: 5000,
+      open: true,
+      livereload: true,
+    })
+  );
 }
 
 module.exports = {
